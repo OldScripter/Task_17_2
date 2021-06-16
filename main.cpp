141dc0768ec9331c2eb4abf0076129d1f95b385b
OldScripter <Oldscripter@gmail.com>
2021-06-16 08:41:58 +0300

    Reverse function is added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 
+void Swap (int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+
+void PrintArray(int* arr)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << *(arr + i) << "\t";
+    }
+    std::cout << "\n";
+    return;
+}
+
+void InitializeArray(int* arr)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        *(arr + i) = i+1;
+    }
+}
+
+int* Reverse (int* arr)
+{
+    for (int i = 0; i < 5; i ++)
+    {
+        Swap(arr + i, arr + 9 - i);
+    }
+    return arr;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int input_arr[10];
+    InitializeArray(input_arr);
+
+    std::cout << "Direct order: \n";
+    PrintArray(input_arr);
+
+    int* output_arr = Reverse(input_arr);
+
+    std::cout << "Reversed order: \n";
+    PrintArray(output_arr);
+    
     return 0;
 }
